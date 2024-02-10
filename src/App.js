fa409f59d686c6287fcb7a1e79084e63f2a7c661
amaldevk <amalvasudev111@gmail.com>
2024-02-10 11:45:48 +0530

    login ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import NewLogin from './components/NewLogin';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div>
-     
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element = {<NewLogin/>} />
+      <Route path='/signup' element = {<SignUp/>} />
+      
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/NewLogin.jsx
@@ -0,0 +1,75 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+const NewLogin = () => {
+    const navigate = useNavigate()
+    const [input,setInput] = new useState(
+        {
+            Email:"",
+            Password:""
+        }
+    )
+    const inputHandler = (event)=>{
+        setInput({...input,[event.target.name]:event.target.value})
+    }
+    const readValues = ()=>{
+        console.log(input)
+        axios.post("http://localhost:3001/api/blog/signin",input).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status == "invalid user") {
+                    alert("invalid email")
+                    setInput(
+                        {
+                            Email:"",
+                            Password:""
+                        }
+                    )
+                } else if(response.data.status == "invalid password"){
+                    alert("invalid password")
+                    setInput(
+                        {
+                            Email:"",
+                            Password:""
+                        }
+                    )
+                }
+                else{
+                    alert("success")
+                    navigate("/add")
+                }
+            }
+        )
+    }
+  return (
+    <div>
+        
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Email ID</label>
+                            <input type="email" className="form-control" name='Email' value={input.Email} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">password</label>
+                            <input type="text" className="form-control" name='Password' value={input.Password} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success" onClick={readValues}>LOGIN</button>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <Link to='/signup'>New users click here</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default NewLogin
